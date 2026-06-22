3f3b6bb74c46e967395fda238abade55346f5f50
terraform-state <github-actions@users.noreply.github.com>
2026-06-22 07:15:01 +0000

    fix: trigger redeploy

--- app/application.py
+++ app/application.py
@@ -4,7 +4,6 @@ import psycopg2
 import psycopg2.extras
 from werkzeug.security import check_password_hash
 from flask import Flask, session, redirect, url_for, request, render_template, abort
-from prometheus_flask_exporter import PrometheusMetrics
 
 
 app = Flask(__name__)
